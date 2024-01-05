a82cee52c0730563265d51cdc965b90827c1d88d
tanque <paoletti.francesco@gmail.com>
2024-01-05 02:24:19 +0100

    feat(schema): add from_file method implementation

--- day-03/src/schema.rs
+++ day-03/src/schema.rs
@@ -1,3 +1,7 @@
+use std::{fs, usize};
+
+use super::Scanner;
+
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Schema {
     schema: Vec<Vec<SchemaElement>>,
@@ -9,7 +13,74 @@ impl Schema {
     }
 
     pub fn from_file(file_path: &str) -> Schema {
-        Schema { schema: Vec::new() }
+        match fs::read_to_string(file_path) {
+            Ok(content) => {
+                //
+                let mut schema_vec: Vec<Vec<SchemaElement>> = Vec::new();
+                let lines: Vec<&str> = content.lines().collect();
+
+                let mut line_number: usize = 0;
+
+                for line in lines {
+                    //println!("line nuber: {:?}", line_number);
+                    //println!("line: {:?}", line);
+
+                    let mut line_vec: Vec<SchemaElement> = Vec::new();
+                    let mut line_scanner = Scanner::new(line);
+                    let mut x_line: usize = 0;
+
+                    line_scanner.scan(|character| {
+                        let y_line = line_number.clone();
+
+                        if character.eq(&'.') {
+                            line_vec.push(SchemaElement::Dot(SchemaElementProps {
+                                position: SchemaPosition {
+                                    x: x_line,
+                                    y: y_line,
+                                },
+                                value: character.to_string(),
+                                width: 1,
+                            }))
+                        };
+
+                        if character.is_alphabetic() {
+                            line_vec.push(SchemaElement::Unknown(SchemaElementProps {
+                                position: SchemaPosition {
+                                    x: x_line,
+                                    y: y_line,
+                                },
+                                value: character.to_string(),
+                                width: 1,
+                            }))
+                        };
+
+                        if character.is_numeric() {
+                            line_vec.push(SchemaElement::Number(SchemaElementProps {
+                                position: SchemaPosition {
+                                    x: x_line,
+                                    y: y_line,
+                                },
+                                value: character.to_string(),
+                                width: 1,
+                            }))
+                        }
+
+                        x_line += 1;
+                        Some(character.clone())
+                    });
+
+                    //println!("line_vec: {:?}", line_vec);
+                    schema_vec.push(line_vec);
+                    line_number += 1;
+                }
+
+                Schema { schema: schema_vec }
+            }
+            Err(err) => {
+                eprintln!("Error in reading file '{}': {}", file_path, err);
+                Schema { schema: Vec::new() }
+            }
+        }
     }
 
     pub fn schema(&self) -> &Vec<Vec<SchemaElement>> {
